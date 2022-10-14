a5e0d1e37f49a211206e3c9b13b4b877f01c08a4
mari-trancoso <mnegraotrancoso@gmail.com>
2022-10-14 16:38:36 -0300

    exercicios feitos com style-components

--- src/Componentes/CardVideo.js
+++ src/Componentes/CardVideo.js
@@ -1,12 +1,17 @@
+import styled from "styled-components";
+import {BoxPaginaPrincipal, Imagem, Titulo4} from "./CardVideoStyle.js"
+
+
+
 function CardVideo(props) {
   function reproduzVideo(props) {
     alert("O vídeo está sendo reproduzido");
   }
   return (
-    <div className="box-pagina-principal" onClick={reproduzVideo}>
-      <img src={props.image1} alt={props.textoAlternativo} />
-      <h4>{props.titulo}</h4>
-    </div>
+    <BoxPaginaPrincipal onClick={reproduzVideo}>
+      <Imagem src={props.image1} alt={props.textoAlternativo} />
+      <Titulo4>{props.titulo}</Titulo4>
+    </BoxPaginaPrincipal>
   );
 }
 
---
+++ src/Componentes/CardVideoStyle.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const BoxPaginaPrincipal = styled.div`
+  display: grid;
+  grid-template-rows: 3fr 1fr;
+  grid-template-columns: 1fr;
+  border-style: solid;
+  border-width: thin;
+  align-items: start;
+  justify-items: stretch;
+`
+
+export const Imagem = styled.img`
+  width: 100%;
+  height: 200px;
+`
+
+export const Titulo4 = styled.h4`
+  margin-left: 20px;
+`
