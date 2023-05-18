8b78c7c28ea8b7315a06b229441e1f77b4169c33
Lu4n-M0ur4 <luan.moura-rj@hotmail.com>
2023-05-18 15:56:21 -0300

    Resolução exercicio styled-components

---
+++ src/Componentes/CardVideo/style.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  display: grid;
+  grid-template-rows: 3fr 1fr;
+  grid-template-columns: 1fr;
+  border-style: solid;
+  border-width: thin;
+  align-items: start;
+  justify-items: stretch;
+
+`
